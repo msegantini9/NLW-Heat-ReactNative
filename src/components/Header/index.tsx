f707125419210a341d16c4f1d3d8356092b9894e
Matheus Segantini de Almeida <m.segantini9@gmail.com>
2021-10-22 22:47:51 -0300

    Ultimas alterações por hoje

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 import LogoSvg from '../../assets/logo.svg'
+import {UserPhoto} from '../UserPhoto'
 
 export function Header(){
   return (
@@ -11,7 +12,13 @@ export function Header(){
 
       <LogoSvg/>
 
-      <Text style={styles.logoutText}>Sair</Text>
+      <UserPhoto imageUri="https://github.com/msegantini9.png"/>
+
+      <TouchableOpacity>
+        <Text style={styles.logoutText}>
+          Sair
+        </Text>
+      </TouchableOpacity>
 
     </View>
   );
