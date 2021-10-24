134f75c1d744cf56c16c47615adf749d4df0d682
Matheus Segantini de Almeida <m.segantini9@gmail.com>
2021-10-23 23:32:45 -0300

    Finalizado :)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NLW-Heat-ReactNative
+
+#ImaginarConstruirTransformar
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native'
+import {View, KeyboardAvoidingView, Platform} from 'react-native'
 import {styles} from './styles'
 import {Header} from '../../components/Header'
 import {MessageList} from '../../components/MessageList'
@@ -11,10 +11,15 @@ export function Home(){
   const {user} = useAuth();
 
   return (
-    <View style={styles.container}>
-      <Header/>
-      <MessageList/>
-      {user? <SendMessageForm/> : <SigninBox/> }
-    </View>
+    <KeyboardAvoidingView 
+      style={{ flex:1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined }
+    >
+      <View style={styles.container}>
+        <Header/>
+        <MessageList/>
+        {user? <SendMessageForm/> : <SigninBox/> }
+      </View>
+    </KeyboardAvoidingView>
   )
 }
